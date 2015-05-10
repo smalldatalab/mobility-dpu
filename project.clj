383fe91a863606e0b4cfdd9318870e4d3504ebad
changun <changun.tw@gmail.com>
2015-05-10 13:47:41 -0400

    advance ver num

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mobility-dpu "0.2.2-SNAPSHOT"
+(defproject mobility-dpu "0.2.4-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
